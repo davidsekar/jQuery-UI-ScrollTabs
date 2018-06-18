8ca3691f10ff58ca1b2c16844ecede82228e3293
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-18 21:53:01 +0000

    Deploy davidsekar/jQuery-UI-ScrollTabs to github.com/davidsekar/jQuery-UI-ScrollTabs.git:gh-pages

---
+++ js/jquery.init.js
@@ -0,0 +1,96 @@
+/*! jquery-ui-scrolltab |  * v1.0.0 |  * https://davidsekar.github.io/jQuery-UI-ScrollTab/ |  * @license MIT */
+/// <reference path="../../node_modules/@types/jquery/index.d.ts" />
+/// <reference path="../../node_modules/@types/jqueryui/index.d.ts" />
+/// <reference path="../ts/jquery.ui.scrolltabs.d.ts" />
+$(function () {
+    // To get the random tabs label with variable length for testing the calculations
+    var keywords = ['Just a tab label', 'Long string', 'Short',
+        'Very very long string', 'tab', 'New tab', 'This is a new tab'];
+    // Original tabs
+    $('#example_0_1').tabs();
+    $('#example_0_2').scrollTabs({
+        activate: function () {
+            console.log('activate triggered');
+        },
+        beforeActivate: function () {
+            console.log('before activate triggered');
+        },
+        scrollOptions: {
+            enableDebug: true,
+            selectTabAfterScroll: false,
+            closable: false,
+            showNavWhenNeeded: false
+        }
+    });
+    $('#example_0').scrollTabs({
+        scrollOptions: {
+            enableDebug: true,
+            selectTabAfterScroll: false,
+            selectTabOnAdd: true
+        }
+    });
+    $('#addRandomTab').on('click', function (e) {
+        var randIdx = Math.floor(Math.random() * keywords.length);
+        $('#example_0').data('uiScrollTabs')
+            .addTab(keywords[randIdx], keywords[randIdx] + '<br>' +
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+            ' Quisque hendrerit vulputate porttitor. Fusce purus leo, faucibus' +
+            ' a sagittis congue, molestie tempus felis. Donec convallis semper enim,' +
+            ' varius sagittis eros imperdiet in. Vivamus semper sem at metus mattis' +
+            ' a aliquam neque ornare. Proin sed semper lacus.');
+    });
+    $('#example_1')
+        .scrollTabs({
+        scrollOptions: {
+            customNavNext: '#n',
+            customNavPrev: '#p',
+            customNavFirst: '#f',
+            customNavLast: '#l',
+            easing: 'swing',
+            enableDebug: false,
+            closable: true,
+            showFirstLastArrows: true,
+            selectTabAfterScroll: true
+        }
+    });
+    $('#example_3').scrollTabs({
+        scrollOptions: {
+            easing: 'swing',
+            enableDebug: false,
+            closable: true,
+            showFirstLastArrows: false,
+            selectTabAfterScroll: true
+        }
+    });
+    $('#example_2').scrollTabs({
+        scrollOptions: {
+            easing: 'swing',
+            enableDebug: false,
+            closable: true,
+            showFirstLastArrows: true,
+            selectTabAfterScroll: true,
+            headerHTML: '<div class="ui-widget-header ui-corner-all"/>',
+            headerScrollHTML: '<div style="background-color:#ccc;" class="ui-scroll-tabs-view"/>',
+            leftArrowWrapperHTML: '<div class="stNavMain stNavMainLeft"/>',
+            customMovePreviousHTML: '<a href="javascript:void(0)"' +
+                ' class="stNavPrevArrow ui-state-active navbutton"' +
+                ' title="Previous">Previous tab</a>',
+            customGotoFirstHTML: '<a href="javascript:void(0)"' +
+                ' class="stNavFirstArrow ui-state-active navbutton" title="First">' +
+                'First tab</a>',
+            rightArrowWrapperHTML: '<div class="stNavMain stNavMainRight"/>',
+            customMoveNextHTML: '<a href="javascript:void(0)"' +
+                ' class="stNavNextArrow ui-state-active navbutton" title="Next">' +
+                'Next tab</a>',
+            customGoToLastHTML: '<a href="javascript:void(0)"' +
+                ' class="stNavLastArrow ui-state-active navbutton" title="Last">' +
+                'Last Tab</a>',
+        }
+    });
+    $('#back-to-top').on('click', function (e) {
+        var body = $('html, body');
+        body.stop().animate({ scrollTop: 0 }, 500, 'swing');
+    });
+});
+
+//# sourceMappingURL=jquery.init.js.map
